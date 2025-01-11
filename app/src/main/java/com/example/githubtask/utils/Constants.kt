a972b3a8a5fc305d652b7008702174bf0c45f361
George-Samaan <georgemichel07@icloud.com>
2025-01-11 21:10:07 +0200

    + updating

--- app/src/main/java/com/example/githubtask/utils/Constants.kt
+++ app/src/main/java/com/example/githubtask/utils/Constants.kt
@@ -2,6 +2,6 @@ package com.example.githubtask.utils
 
 object Constants {
     const val BASE_URL = "https://api.github.com/"
-    const val TOKEN = "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"
+    const val TOKEN = "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"
     const val REQUEST_CODE_WIFI = 1001
 }
